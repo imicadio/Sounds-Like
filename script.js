67f22e7ccc8a9430bfd9e10f9a46ba124d51b528
imicadio <mrjesio@gmail.com>
2018-02-04 17:22:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-window.addEventListener('keydown', function(e){
+/* window.addEventListener('keydown', function(e){
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	console.log(audio);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if(!audio) return; // zatrzymanie funkcji 
+	audio.currentTime = 0; // przewija do początku  currentTime ustawia pozycję czasową 
+	audio.play();
+	key.classList.add('playing');
 });
+
+*/
+function playSound(e){
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if(!audio) return; // zatrzymanie funkcji 
+	audio.currentTime = 0; // przewija do początku  currentTime ustawia pozycję czasową 
+	audio.play();
+	key.classList.add('playing');
+}
+
+
+
+function removeTransition(e){
+		// console.log(e);
+		if (e.propertyName !== 'transform') return;
+		console.log(this);
+		this.classList.remove('playing');
+	}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
+
+// console.log(keys);
+// console.log(keys[0]);
+
+// setTimeout(function(e){document.querySelector(`.key[data-key="${e.keyCode}"]`).classList.remove('playing')}, 1200)
